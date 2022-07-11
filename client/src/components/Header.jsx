9af86e1f2f5c884e43a6c9c82b67a65e5e73d452
malek-haddar <malek.haddar1@esprit.tn>
2022-07-11 11:33:54 +0100

    update notif2

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -67,21 +67,6 @@ function Header() {
                           <div className="cart-ticket">
                             <Notification />
                           </div>
-                          {/* <li>
-                            <span class="flex h-3 w-3">
-                              <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                              <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-                            </span>
-                            <a href="#">notification</a>
-                            <ul className="submenu">
-                              <li>
-                                <a href="">All Speakers</a>
-                              </li>
-                              <li>
-                                <a href="">Speaker Details</a>
-                              </li>
-                            </ul>
-                          </li> */}
                         </>
                       )}
 
