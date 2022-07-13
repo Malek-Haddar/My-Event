a38b8b894500f9069898c2948f18372a499a710c
malek-haddar <malek.haddar1@esprit.tn>
2022-07-13 17:55:22 +0100

    fix middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,39 +1,67 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
-// const secret = 'test';
-
+const secret = 'test';
 
 export const auth = async (req, res, next) => {
-    let token
-
+    let token;
+  
     if (
       req.headers.authorization &&
       req.headers.authorization.startsWith("Bearer")
     ) {
       try {
-        // Get token from header
+        console.log({ token });
         token = req.headers.authorization.split(" ")[1];
-
-        // Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Get user from the token
+  
+        if (!token) return "Not authorized, token failed";
+        const decoded = jwt.verify(token, secret);
+  
         req.user = await User.findById(decoded.id).select("-password");
-
+  
         next();
       } catch (error) {
-        console.log(error);
-
+        console.error(error);
         return "Not authorized, token failed";
       }
     }
-
+  
     if (!token) {
       res.status(401);
       return "Not authorized, no token";
     }
-  }
+  };
+  
+// export const auth = async (req, res, next) => {
+//     let token
+
+//     if (
+//       req.headers.authorization &&
+//       req.headers.authorization.startsWith("Bearer")
+//     ) {
+//       try {
+//         // Get token from header
+//         token = req.headers.authorization.split(" ")[1];
+
+//         // Verify token
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//         // Get user from the token
+//         req.user = await User.findById(decoded.id).select("-password");
+
+//         next();
+//       } catch (error) {
+//         console.log(error);
+
+//         return "Not authorized, token failed";
+//       }
+//     }
+
+//     if (!token) {
+//       res.status(401);
+//       return "Not authorized, no token";
+//     }
+//   }
   
   
 
