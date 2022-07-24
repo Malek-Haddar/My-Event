818edaefcc9d8bb7c51cf45b931ac4c14cb8a3a5
malek-haddar <malek.haddar1@esprit.tn>
2022-07-24 12:36:17 +0100

    group session with day

--- client/src/components/chat/axios.js
+++ client/src/components/chat/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: process.env.API_URL,
   baseURL: "https://scouts-tunisienne.herokuapp.com/",
   // baseURL: "http://localhost:5000/",
 });
