ac41adf3f9df419fd1fb3109af9c361280c7ae68
malek haddar <malekhaddar8@gmail.com>
2023-01-27 13:14:27 +0100

    fix video tuto

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -53,10 +53,17 @@ function Session() {
             <div className="col-lg-8">
               <div className="about-image">
                 <iframe
-                  src="https://drive.google.com/file/d/1IzHryIRmEBDAuIfvKFCx4bpf_oRoXZLE/preview"
-                  className="w-full"
-                  height="480"
-                  allow="autoplay"
+                  className="md:w-full w-full"
+                  style={{ height: "38rem" }}
+                  src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FI.WATCH.Organization%2Fvideos%2F1350888112403862%2F&show_text=false&t=0"
+                  // width="100%"
+                  // height="560"
+                  // style={{ border: none; overflow: hidden }}
+                  scrolling="no"
+                  frameborder="0"
+                  allowfullscreen="true"
+                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                  allowFullScreen="true"
                 ></iframe>
               </div>
             </div>
